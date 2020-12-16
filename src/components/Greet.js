d7b89a2ece8ae3964e55b975832b25e1685527ad
Ali <sahbaj.ali@rakuten.com>
2020-12-16 13:59:04 +0530

    Children

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -4,6 +4,7 @@ const Greet=(props)=>{
     console.log(props)
     return (
         //using a main div tag because we can return only one element
+        //use props.children when we are not sure of the names used in props. If nothing is there in children nothing will be seen
     <div>
         <h1>Hello {props.name} aka {props.heroname}</h1>
         {props.children}
